b8769d84050667d038ea6c65601e35dec4f94d37
中井威志 <tksmac@nakaitakeshishis-MacBook-Air.local>
2024-02-09 03:57:33 +0900

    a

--- encn_Cambridge.js
+++ encn_Cambridge.js
@@ -42,109 +42,117 @@ class encn_Cambridge {
       let parser = new DOMParser();
       doc = parser.parseFromString(data, 'text/html');
     } catch (err) {
+      console.error(err);
       return [];
     }
 
-    let entries = doc.querySelectorAll('.pr .entry-body__el') || [];
-    for (const entry of entries) {
-      let definitions = [];
-      let audios = [];
+    try {
+      let entries = doc.querySelectorAll('.pr .entry-body__el') || [];
+      for (const entry of entries) {
+        let definitions = [];
+        let audios = [];
 
-      let expression = T(entry.querySelector('.headword'));
-      let reading = '';
-      let readings = entry.querySelectorAll('.pron .ipa');
-      if (!Array.isArray(readings) || readings.length === 0) {
-        console.log('no readings');
-      } else if (readings.length < 2) {
-        console.log('not enough readings');
-      } else {
-        let reading_us = T(readings[0]);
-        let reading_uk = T(readings[1]);
-        reading =
-          reading_us || reading_uk
-            ? `US[${reading_us}] UK[${reading_uk}] `
-            : '';
-      }
-      let pos = T(entry.querySelector('.posgram'));
-      pos = pos ? `<span class='pos'>${pos}</span>` : '';
-      audios[0] = entry.querySelector('.uk.dpron-i source');
-      audios[0] = audios[0]
-        ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src')
-        : '';
-      //audios[0] = audios[0].replace('https', 'http');
-      audios[1] = entry.querySelector('.us.dpron-i source');
-      audios[1] = audios[1]
-        ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src')
-        : '';
-      //audios[1] = audios[1].replace('https', 'http');
-
-      let sensbodys = entry.querySelectorAll('.sense-body') || [];
-      for (const sensbody of sensbodys) {
-        let sensblocks = sensbody.childNodes || [];
-        for (const sensblock of sensblocks) {
-          let phrasehead = '';
-          let defblocks = [];
-          if (
-            sensblock.classList &&
-            sensblock.classList.contains('phrase-block')
-          ) {
-            phrasehead = T(sensblock.querySelector('.phrase-title'));
-            phrasehead = phrasehead
-              ? `<div class="phrasehead">${phrasehead}</div>`
+        let expression = T(entry.querySelector('.headword'));
+        let reading = '';
+        let readings = entry.querySelectorAll('.pron .ipa');
+        if (!Array.isArray(readings) || readings.length === 0) {
+          console.log('no readings');
+        } else if (readings.length < 2) {
+          console.log('not enough readings');
+        } else {
+          let reading_us = T(readings[0]);
+          let reading_uk = T(readings[1]);
+          reading =
+            reading_us || reading_uk
+              ? `US[${reading_us}] UK[${reading_uk}] `
               : '';
-            defblocks = sensblock.querySelectorAll('.def-block') || [];
-          }
-          if (
-            sensblock.classList &&
-            sensblock.classList.contains('def-block')
-          ) {
-            defblocks = [sensblock];
-          }
-          if (defblocks.length <= 0) continue;
+        }
+        let pos = T(entry.querySelector('.posgram'));
+        pos = pos ? `<span class='pos'>${pos}</span>` : '';
+        audios[0] = entry.querySelector('.uk.dpron-i source');
+        audios[0] = audios[0]
+          ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src')
+          : '';
+        //audios[0] = audios[0].replace('https', 'http');
+        audios[1] = entry.querySelector('.us.dpron-i source');
+        audios[1] = audios[1]
+          ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src')
+          : '';
+        //audios[1] = audios[1].replace('https', 'http');
 
-          // make definition segement
-          for (const defblock of defblocks) {
-            let eng_tran = T(defblock.querySelector('.ddef_h .def'));
-            let chn_tran = T(defblock.querySelector('.def-body .trans'));
-            if (!eng_tran) continue;
-            let definition = '';
-            eng_tran = `<span class='eng_tran'>${eng_tran.replace(
-              RegExp(expression, 'gi'),
-              `<b>${expression}</b>`
-            )}</span>`;
-            chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
-            let tran = `<span class='tran'>${eng_tran}${chn_tran}</span>`;
-            definition += phrasehead ? `${phrasehead}${tran}` : `${pos}${tran}`;
-
-            // make exmaple segement
-            let examps = defblock.querySelectorAll('.def-body .examp') || [];
-            if (examps.length > 0 && this.maxexample > 0) {
-              definition += '<ul class="sents">';
-              for (const [index, examp] of examps.entries()) {
-                if (index > this.maxexample - 1) break; // to control only 2 example sentence.
-                let eng_examp = T(examp.querySelector('.eg'));
-                let chn_examp = T(examp.querySelector('.trans'));
-                definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(
-                  RegExp(expression, 'gi'),
-                  `<b>${expression}</b>`
-                )}</span><span class='chn_sent'>${chn_examp}</span></li>`;
-              }
-              definition += '</ul>';
+        let sensbodys = entry.querySelectorAll('.sense-body') || [];
+        for (const sensbody of sensbodys) {
+          let sensblocks = sensbody.childNodes || [];
+          for (const sensblock of sensblocks) {
+            let phrasehead = '';
+            let defblocks = [];
+            if (
+              sensblock.classList &&
+              sensblock.classList.contains('phrase-block')
+            ) {
+              phrasehead = T(sensblock.querySelector('.phrase-title'));
+              phrasehead = phrasehead
+                ? `<div class="phrasehead">${phrasehead}</div>`
+                : '';
+              defblocks = sensblock.querySelectorAll('.def-block') || [];
             }
-            definition && definitions.push(definition);
+            if (
+              sensblock.classList &&
+              sensblock.classList.contains('def-block')
+            ) {
+              defblocks = [sensblock];
+            }
+            if (defblocks.length <= 0) continue;
+
+            // make definition segement
+            for (const defblock of defblocks) {
+              let eng_tran = T(defblock.querySelector('.ddef_h .def'));
+              let chn_tran = T(defblock.querySelector('.def-body .trans'));
+              if (!eng_tran) continue;
+              let definition = '';
+              eng_tran = `<span class='eng_tran'>${eng_tran.replace(
+                RegExp(expression, 'gi'),
+                `<b>${expression}</b>`
+              )}</span>`;
+              chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
+              let tran = `<span class='tran'>${eng_tran}${chn_tran}</span>`;
+              definition += phrasehead
+                ? `${phrasehead}${tran}`
+                : `${pos}${tran}`;
+
+              // make exmaple segement
+              let examps = defblock.querySelectorAll('.def-body .examp') || [];
+              if (examps.length > 0 && this.maxexample > 0) {
+                definition += '<ul class="sents">';
+                for (const [index, examp] of examps.entries()) {
+                  if (index > this.maxexample - 1) break; // to control only 2 example sentence.
+                  let eng_examp = T(examp.querySelector('.eg'));
+                  let chn_examp = T(examp.querySelector('.trans'));
+                  definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(
+                    RegExp(expression, 'gi'),
+                    `<b>${expression}</b>`
+                  )}</span><span class='chn_sent'>${chn_examp}</span></li>`;
+                }
+                definition += '</ul>';
+              }
+              definition && definitions.push(definition);
+            }
           }
         }
+        let css = this.renderCSS();
+        notes.push({
+          css,
+          expression,
+          reading,
+          definitions,
+          audios,
+        });
       }
-      let css = this.renderCSS();
-      notes.push({
-        css,
-        expression,
-        reading,
-        definitions,
-        audios,
-      });
+      return notes;
+    } catch (err) {
+      console.error(err);
+      return [];
     }
-    return notes;
   }
 
   renderCSS() {
